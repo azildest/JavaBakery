0ccc1a7c24ac476536bfaa107ed6335247ce948b
azildest <azildest@users.noreply.github.com>
2022-12-24 19:38:25 +0700

    add akun feature

--- src/main/java/com/itenas/javabakery/service/AkunService.java
+++ src/main/java/com/itenas/javabakery/service/AkunService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.itenas.javabakery.service;
+package com.itenas.javabakery.service; 
 
 //import com.itenas.javabakery.pojo.Member/Customer;
 import com.itenas.javabakery.repository.AkunRepository;
